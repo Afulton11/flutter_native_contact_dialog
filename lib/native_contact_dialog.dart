b2bc9382acaef207303eb498f14f30713df2113b
Andrew Fulton <afulton20@gmail.com>
2019-02-10 19:51:30 -0600

    Run flutter format

--- lib/native_contact_dialog.dart
+++ lib/native_contact_dialog.dart
@@ -5,7 +5,8 @@ import 'package:flutter/services.dart';
 
 /// Contains static functions for invoking native contact dialogs
 class NativeContactDialog {
-  static const MethodChannel _channel = const MethodChannel('github.com.afulton11.plugins/native_contact_dialog');
+  static const MethodChannel _channel =
+      const MethodChannel('github.com.afulton11.plugins/native_contact_dialog');
 
   /// Displays the [contact] in a native add contact dialog.
   static Future<String> addContact(Contact contact) async {
